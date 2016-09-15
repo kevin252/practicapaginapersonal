177ab1b2f2c196b06f123986b1d4d154618f7af3
Kevin Cardenas Acevedo <Kvin252@hotmail.com>
2016-09-14 20:39:28 -0500

    Crear clase persona
    Se crea ua clase persona.java con los siguientes atributos: usuario y clase
    
    Tipo: nueva funcionalidad

---
+++ appjava/src/modelo/Persona.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author kvin2
+ */
+public class Persona {
+public Persona() {
+    }
+
+    public Persona(String usuario, String contraseña) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+    
+
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    private String usuario;
+    private String contraseña;
+
+    
+    
+    
+}
