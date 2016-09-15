8487dfb1911f3a1412a0abfc0c07e517a3e2dad3
Kevin Cardenas Acevedo <Kvin252@hotmail.com>
2016-09-14 21:00:21 -0500

    Nuevas funcionalidad
    Se crea la clase RepositorioMySql y se adicionan dos metodos:obtenerCantidadPersonas y adicionarPersonas
    
    Tipo:nuevas funcionalidades

---
+++ appjava/src/dao/RepositorioMySql.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.ArrayList;
+import modelo.Persona;
+
+/**
+ *
+ * @author kvin2
+ */
+public class RepositorioMySql {
+ private ArrayList<Persona> personas;
+
+    public RepositorioMySql() {
+        personas= new ArrayList<>();
+    }
+
+    public ArrayList<Persona> getPersonas() {
+        return personas;
+    }
+
+    public void setPersonas(ArrayList<Persona> personas) {
+        this.personas = personas;
+    }
+ 
+public int obtenerCantidadPersonas(){
+    return this.personas.size();
+}
+
+public boolean adicionarPersona(Persona p){
+    boolean estado= this.personas.add(p);
+    return estado;
+}
+
+
+}
