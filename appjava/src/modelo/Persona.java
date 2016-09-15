37aec79910862604419a3438cee823cd476c1384
Kevin Cardenas Acevedo <Kvin252@hotmail.com>
2016-09-14 20:42:48 -0500

    Se modifico la clase persona
    Se añadio a la clase persona los siguientes atributos: nombre y apellido
    
    Tipo:Mejora

--- appjava/src/modelo/Persona.java
+++ appjava/src/modelo/Persona.java
@@ -10,15 +10,37 @@ package modelo;
  * @author kvin2
  */
 public class Persona {
-public Persona() {
+
+    private String usuario;
+    private String contraseña;
+    private String apellido;
+    private String nombre;
+
+    public Persona() {
     }
 
-    public Persona(String usuario, String contraseña) {
+    public Persona(String usuario, String contraseña, String apellido, String nombre) {
         this.usuario = usuario;
         this.contraseña = contraseña;
+        this.apellido = apellido;
+        this.nombre = nombre;
     }
-    
 
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
     public String getUsuario() {
         return usuario;
@@ -35,10 +57,5 @@ public Persona() {
     public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
     }
-    private String usuario;
-    private String contraseña;
 
-    
-    
-    
 }
